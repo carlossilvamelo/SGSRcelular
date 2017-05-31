10c238b8433f053439f04e65ad0f48faa1d33b02
carlos henrique da silva melo <carlossilvamelo7@hotmail.com>
2017-05-31 14:26:15 -0300

    estrutura do projeto

---
+++ src/main/java/com/SGSRcelular/SgsRcelularApplication.java
@@ -0,0 +1,12 @@
+package com.SGSRcelular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgsRcelularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgsRcelularApplication.class, args);
+	}
+}
